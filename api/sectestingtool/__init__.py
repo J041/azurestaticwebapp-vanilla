540dd815d66646bbcc30418b1eeae0777f145322
J041 <46340016+J041@users.noreply.github.com>
2022-07-15 15:07:03 +0800

    Update __init__.py

--- api/sectestingtool/__init__.py
+++ api/sectestingtool/__init__.py
@@ -5,28 +5,24 @@ import azure.functions as func
 import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    x = []
     logging.info('Python HTTP trigger function processed a request. Download sec testing tools')
     if not os.path.exists("linpeas.sh"):
-        subprocess.check_output("curl https://github.com/carlospolop/PEASS-ng/releases/download/20220710/linpeas.sh", shell=True)
-        return func.HttpResponse(
-                "This HTTP triggered function executed successfully. Downloaded linpeas.",
-                status_code=200
-            )
+        subprocess.check_output("curl -L https://github.com/carlospolop/PEASS-ng/releases/download/20220710/linpeas.sh -o linpeas.sh", shell=True)
+        x.append("linpeas")
     if not os.path.exists("cdk_linux_386"):
-        subprocess.check_output("curl https://github.com/cdk-team/CDK/releases/download/v1.3.0/cdk_linux_386", shell=True)
-        return func.HttpResponse(
-                "This HTTP triggered function executed successfully. Downloaded cdk.",
-                status_code=200
-            )
+        subprocess.check_output("curl -L https://github.com/cdk-team/CDK/releases/download/v1.3.0/cdk_linux_386 -o cdk_linux_386", shell=True)
+        x.append("cdk")
     if not os.path.exists("nmap-7.92.tar.bz2"):
-        subprocess.check_output("curl https://nmap.org/dist/nmap-7.92.tar.bz2", shell=True)
+        subprocess.check_output("curl https://nmap.org/dist/nmap-7.92.tar.bz2 -o nmap-7.92.tar.bz2", shell=True)
         subprocess.check_output("bzip2 -cd nmap-7.92.tar.bz2 | tar xvf -", shell=True)
         subprocess.check_output("cd nmap-7.92; ./configure", shell=True)
         subprocess.check_output("make", shell=True)
-        return func.HttpResponse(
-                "This HTTP triggered function executed successfully. Downloaded nmap.",
-                status_code=200
-            )
+        x.append("nmap")
+    return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Downloaded "+str(x),
+            status_code=200
+        )
 
 
 
